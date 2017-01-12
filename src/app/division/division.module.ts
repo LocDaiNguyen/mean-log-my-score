7a61bccd97886470a0d8c8852116ce8195b3c567
Loc Nguyen <loc.nguyen.33@gmail.com>
2017-01-12 13:52:19 -0600

    format league|divison|.module.ts

--- src/app/division/division.module.ts
+++ src/app/division/division.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { DivisionComponent } from './division.component';
-
-import { DivisionRoutingModule } from './division-routing.module';
 import { DivisionListComponent } from './division-list/division-list.component';
 import { DivisionNewComponent } from './division-new/division-new.component';
 import { DivisionUpdateComponent } from './division-update/division-update.component';
+
+import { DivisionRoutingModule } from './division-routing.module';
 
 @NgModule({
   imports: [
--- src/app/league/league.module.ts
+++ src/app/league/league.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { LeagueRoutingModule } from './league-routing.module';
-
 import { LeagueComponent } from './league.component';
 import { LeagueListComponent } from './league-list/league-list.component';
 import { LeagueNewComponent } from './league-new/league-new.component';
 import { LeagueUpdateComponent } from './league-update/league-update.component';
+
+import { LeagueRoutingModule } from './league-routing.module';
 
 
 @NgModule({
